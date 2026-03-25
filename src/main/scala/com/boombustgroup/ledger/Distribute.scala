05a6dfe036edf5592e84df27ef97f29f2e64b0b6
Mateusz Maciaszek <mateusz.maciaszekhpc@gmail.com>
2026-03-25 17:30:09 +0100

    Format distribution implementation

--- src/main/scala/com/boombustgroup/ledger/Distribute.scala
+++ src/main/scala/com/boombustgroup/ledger/Distribute.scala
@@ -2,9 +2,8 @@ package com.boombustgroup.ledger
 
 /** Proportional distribution with floor-based residual plugging.
   *
-  * Distributes `total` across N recipients according to `shares`. All but the last recipient get their floored proportional
-  * allocation. The last recipient absorbs the residual, guaranteeing:
-  * distribute(total, shares).sum == total
+  * Distributes `total` across N recipients according to `shares`. All but the last recipient get their floored proportional allocation. The
+  * last recipient absorbs the residual, guaranteeing: distribute(total, shares).sum == total
   *
   * This is exact by construction (Long addition). No tolerance needed.
   */
@@ -28,8 +27,8 @@ object Distribute:
     require(shares.forall(_ >= 0L), "Shares must be non-negative")
     require(shareSum > 0, "Share sum must be positive")
     val shareSumBigInt = BigInt(shareSum)
-    var allocated = 0L
-    var i         = 0
+    var allocated      = 0L
+    var i              = 0
     while i < n - 1 do
       results(i) = ((BigInt(total) * BigInt(shares(i))) / shareSumBigInt).toLong
       allocated += results(i)
